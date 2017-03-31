8b0d628e9207c199dcb3b9661259926ecdd833ae
brady-kimball <kimball.brady@gmail.com>
2017-03-30 19:26:50 -0700

    Create map class skeleton

---
+++ W1D5/map.rb
@@ -0,0 +1,22 @@
+class Map
+  def initialize
+    #initializes variables
+  end
+
+  def assign(key, value)
+    # Assigns key value pair if key doesn't exist
+    # Updates key value pair if key does exist
+  end
+
+  def lookup(key)
+    # lookup key and return value, or nil if key isn't found
+  end
+
+  def remove(key)
+    # delete key value pair from map
+  end
+
+  def show
+    # return list of keys/values
+  end
+end
