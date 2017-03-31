67ce5bd57ec6faacecdca5d1370141391925a4e3
brady-kimball <kimball.brady@gmail.com>
2017-03-30 19:21:46 -0700

    Adds Queue class skeleton

---
+++ W1D5/queue.rb
@@ -0,0 +1,17 @@
+class Queue
+  def initialize
+    #initialize queue
+  end
+
+  def enqueue(el)
+    #pushes variable to end of queue
+  end
+
+  def dequeue
+    #shifts variable from front of queue
+  end
+
+  def show
+    #returns queue
+  end
+end
